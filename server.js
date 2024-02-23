2852d0a62d8e94795a90e05ca8c1f16c8059c018
Tal Cohen <talco4088@gmail.com>
2024-02-23 19:25:06 +0200

    fix the mongodb key

--- server.js
+++ server.js
@@ -75,6 +75,8 @@ app.get('/api/code', async (req, res) => {
     console.log("aaaaaaaaaaaaaaaaaaaaaaa");
     console.log('mongoDBKey', mongoDBKey);
     try {
+        console.log("AAAAAAAAAAAAAAA");
+        console.log('client', client);
         const db = client.db('codeDB')
         console.log("bbbbbbbbbbbbbbbbbb");
         const coll = db.collection('code')
