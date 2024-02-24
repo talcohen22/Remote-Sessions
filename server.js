b56ffbe6dd237571d73e3f79f0339a317d97e3e8
Tal Cohen <talco4088@gmail.com>
2024-02-24 11:52:50 +0200

    make tests for the connection between the frontend and backend

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ if (process.env.NODE_ENV === 'production') {
 
 //connection to mongoDB
 // const uri = "mongodb://0.0.0.0:27017/" //for local usage
+console.log('MMMMMMMMMMMMMMMMMMMMMMMMMM');
+console.log('process.env.mongoURI0', process.env.mongoURI);
 const client = new MongoClient(process.env.mongoURI) //for global usage
 // const client = new MongoClient(mongoDBKey.mongoURI) //for local usage
 // const client = new MongoClient(uri)
